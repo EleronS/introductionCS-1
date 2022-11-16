3a423e9aa5d9e183766dea5d4a58be679d06f1b6
Valeev <eleronsmarya@gmail.com>
2022-11-16 10:28:22 +0300

    RealHomework

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,17 +1,17 @@
 ﻿//03. С клавиатуры вводятся два числа a и b. Найти максимальное из них.
 int a;
 int b;
-System.Console.Write("Введите первое число : ");
+System.Console.Write("Введите первое число (a) : ");
 string? s = Console.ReadLine();
 a = Convert.ToInt32(s);
-System.Console.Write("Введите второе число : ");
+System.Console.Write("Введите второе число (b): ");
 string? d = Console.ReadLine();
 b = Convert.ToInt32(d);
 if(a>b)
 {
-    System.Console.WriteLine($"Максимальное число :{a}");
+    System.Console.WriteLine($"Максимальное число (a) :{a}");
 }
 else
 {
-    System.Console.WriteLine($"Максимальное число :{b}");
+    System.Console.WriteLine($"Максимальное число (b) :{b}");
 }
---
+++ 05/Program.cs
@@ -0,0 +1,25 @@
+﻿// С клавиатуры вводятся три числа, найти максимальное из них.
+int a;
+int b;
+int c;
+System.Console.Write("Введите первое число : ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+System.Console.Write("Введите второе число : ");
+string? d = Console.ReadLine();
+b = Convert.ToInt32(d);
+System.Console.Write("Введите третье число : ");
+string? e = Console.ReadLine();
+c = Convert.ToInt32(e);
+if(a>b & a>c)
+{
+     System.Console.WriteLine($"Максимальное число :{a}");
+}
+if(b>a & b>c)
+{
+     System.Console.WriteLine($"Максимальное число :{b}");
+}
+if(c>a & c>b)
+{
+     System.Console.WriteLine($"Максимальное число :{c}");
+}
