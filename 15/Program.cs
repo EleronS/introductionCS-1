c8066ec421276bc5781d5e48b2df7a363b3b1df3
Valeev <eleronsmarya@gmail.com>
2022-11-16 23:37:29 +0300

    RealHomeWork

---
+++ 15/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цыфру числа или сообщить, что ее нет(Вывести:NO).
+ int a;
+ int b;
+ int c;
+System.Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+b=a%10;
+c=a%100*10/100;
+if(a<100)
+{
+    System.Console.WriteLine("NO");
+}
+else
+{
+    System.Console.WriteLine(b);
+}
