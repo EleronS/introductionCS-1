4d1558b3b76c8ba25cbaaee8277062b672d0cd65
Valeev <eleronsmarya@gmail.com>
2022-11-28 01:01:04 +0300

    Classwork4

---
+++ BasicAlgorithms/005-switch/Program.cs
@@ -0,0 +1,28 @@
+﻿// С клавиатуры вводится число - день недели. Определить день недели.
+int day=1;
+if(day==1)System.Console.WriteLine("Понедельник");
+else
+     if(day==2)System.Console.WriteLine("Вторник");
+         else
+             if(day==3)System.Console.WriteLine("Среда");
+
+switch(day)
+{
+    case 1: 
+    System.Console.WriteLine("Понедельник");
+    break;
+    case 2: 
+    System.Console.WriteLine("Вторник");
+    break;
+    case 3: 
+    System.Console.WriteLine("Среда");
+    break;
+    default:
+    System.Console.WriteLine("Не правильный ввод");
+    break;
+
+    metka1:
+
+    goto metka1;//оператор безусловного перехода. GOTO - нельзя использовать!
+
+}
