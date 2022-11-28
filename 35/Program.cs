37591e4f065f3be59443a581bd051e969402d6b2
Valeev <eleronsmarya@gmail.com>
2022-11-28 19:18:50 +0300

    Homework4

---
+++ 35/Program.cs
@@ -0,0 +1,31 @@
+﻿// 35. Написать программу замены элементов массива на противоположные
+int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+    int[] a=new int[size];
+    Random random = new Random();
+    for(int i=0;i<size;i++)
+    a[i]=random.Next(min, max+1);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],3}");
+}
+void PrintPosToNeg(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+       if(a[i]<0)
+    System.Console.Write($"{a[i]-a[i]*2,3}");
+       else
+       System.Console.Write($"{0-a[i],3}");
+}
+
+int[] a=RandomIntArray(8,-9,9);
+
+Print (a);
+System.Console.WriteLine();
+PrintPosToNeg(a);
+
+
---
+++ 38/Program.cs
@@ -0,0 +1,29 @@
+﻿// 38. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] RandomIntArray(int size=122,int min=0,int max=122)
+{
+    int[] a=new int[size];
+    Random random = new Random();
+    for(int i=0;i<size;i++)
+    a[i]=random.Next(min, max+1);
+    return a;
+}
+int CountPositions(int[] a)
+{
+    int s=0;
+    for(int i=0;i<a.Length;i++)
+       if(a[i]>9 && a[i]<100)
+       s=s+1;
+       return s;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],5}");
+}
+int[] a=RandomIntArray(10,0,123);
+
+Print (a);
+System.Console.WriteLine();
+System.Console.WriteLine(CountPositions(a));
