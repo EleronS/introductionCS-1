f1255194b4e3af7311d76d849ec1ea26296be98e
Valeev <eleronsmarya@gmail.com>
2022-12-02 01:07:58 +0300

    Homework5

---
+++ 54/Program.cs
@@ -0,0 +1,24 @@
+﻿// 54.С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+System.Console.WriteLine( "Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ 
+
+
+    int a =1;
+    int b =1;
+    int c;
+    for(int i=0;i<n;i++)
+    if(i==0)System.Console.Write($"{0} ");
+    else
+    {
+       System.Console.Write($" {a} ");
+       c=a+b;
+       a=b;
+       b=c;
+
+    }       
+
+
+
+
