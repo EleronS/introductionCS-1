a34f5cea9d89af896a19d72b695a3b091cb44109
Valeev <eleronsmarya@gmail.com>
2022-12-02 15:07:08 +0300

    Homework5

---
+++ 56/Program.cs
@@ -0,0 +1,24 @@
+﻿// 56.Написать программу копирования массива.
+System.Console.Write("Введите длину массива: ");
+int n= Convert.ToInt32(Console.ReadLine());
+int[]a= new int[n];
+System.Console.WriteLine("Введите элементы массива: ");
+for(int i=0;i<n;i++)
+a[i]=Convert.ToInt32(Console.ReadLine());
+
+int CopyArray(int[]c)
+{
+    for(int i=0;i<a.Length;i++)
+    c[i]=a[i];
+    return c.Length;
+}
+
+void Print(int[] c)
+{
+    System.Console.WriteLine("Копия массива");
+    for(int i=0;i<c.Length;i++)
+    System.Console.Write($"{c[i],5}");
+}
+int[]c= new int[a.Length];
+CopyArray(c);
+Print(c);
