fe53bfab220298295808ed4515fa9a8ca1019f66
Valeev <eleronsmarya@gmail.com>
2022-12-06 23:15:49 +0300

    Classwork72

---
+++ 73/Program.cs
@@ -0,0 +1,14 @@
+﻿// 73.Найти сумму элементов от M до N, N и M заданы
+
+
+//6
+
+int Sum(int m,int n)
+{
+    if(m==n) return m;
+    else
+      return Sum(m+1,n)+m;
+}
+System.Console.WriteLine(Sum(2,4));
+//анология
+System.Console.WriteLine((2+4)*3/2);
---
+++ BasicAlgorithms/032-Sum digital(Сумма цифр)/Program.cs
@@ -0,0 +1,12 @@
+﻿// Сумма цифр. 
+//!!!!!Punto switcher
+
+
+int SumDigit(int N)
+{
+  if(N==0) return 0;
+  else
+     return SumDigit(N/10)+N%10;
+}
+
+System.Console.WriteLine(SumDigit(123));
