d2d98f0bc66f553f2f68352d7182943bb6dbc539
Valeev <eleronsmarya@gmail.com>
2022-12-10 17:57:44 +0300

    Classwork8

---
+++ 83/Program.cs
@@ -0,0 +1,17 @@
+﻿//83. Подсчитать количество цифр среди вводимых с клавиатуры символов
+
+//for
+/*
+string s=Console.ReadLine();
+int k=0;
+for(int i=0;i<s.Length;i++)
+    if(char.IsDigit(s[i])) k++;
+    System.Console.WriteLine(k);
+*/
+
+//foreach
+    string a=Console.ReadLine();
+int d=0;
+foreach(char c in a)
+    if(char.IsDigit(c)) d++;
+    System.Console.WriteLine(d);
